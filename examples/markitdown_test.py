1ce82053b8776a20fc9b8cf7ec75aa96275b4f27
yanghan-cyber <yanghan1107@outlook.com>
2025-12-10 22:01:57 +0800

    chore(examples): 删除过时示例文件
    删除 `markitdown_test.py` 和 `use_store_checkpoint.py` 两个示例文件，这些文件已不再适用于当前项目架构。

--- examples/markitdown_test.py
+++
@@ -1,5 +0,0 @@
-from markitdown import MarkItDown
-md = MarkItDown(enable_plugins=False) # Set to True to enable plugins
-
-res = md.convert("http://jyt.hunan.gov.cn/jyt/sjyt/hnzxxjsfzzx/jsfzzxtzgg/201912/10783971/files/be971fe3dbf24ab988f0641314c9e91a.pdf")
-print(res.text_content)
